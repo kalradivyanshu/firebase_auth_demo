6a292d3e222315064bacfc142f03cf4cc376a4cb
Divyanshu Kalra <kalradivyanshu@gmail.com>
2023-10-14 22:31:48 +0530

    first commit

---
+++ download_firebase_auth.sh
@@ -0,0 +1,6 @@
+cd .\public\__\auth\
+wget https://reslashstaging.firebaseapp.com/__/auth/handler
+wget https://reslashstaging.firebaseapp.com/__/auth/handler.js
+wget https://reslashstaging.firebaseapp.com/__/auth/experiments.js
+wget https://reslashstaging.firebaseapp.com/__/auth/iframe
+wget https://reslashstaging.firebaseapp.com/__/auth/iframe.js
